e438dd8997ba9e402519775d2e5336fe4c39a879
Igor Vavilin <igorvv.vn@gmail.com>
2021-07-22 17:39:37 +0300

    Добавил заголовок запроса в функции fetchConfig

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,9 @@
 const fetchConfig = async (url, form) => {
     let response = await fetch(url, {
         method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(Object.fromEntries(new FormData(form)))
     })
 
